254265a74aa441f5388aeffdd9d90083613f505e
nibeon <nibeon@gmail.com>
2018-02-23 13:41:30 +0200

    Дошел до главы "встроенные теги" - https://metanit.com/python/django/2.7.php

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path
+
+from django.views.generic import TemplateView
+
 from firstapp import views
 
 urlpatterns = [
     path('', views.index),
-    path('about/', views.about),
-    path('contact/', views.contact),
+    path('about/', TemplateView.as_view(template_name="about.html",
+        extra_context={"header": "О сайте"})),
+    path('contact/', TemplateView.as_view(template_name="contact.html")),
     path('details/', views.details),
+    path('home/', TemplateView.as_view(template_name="home.html")),
+    path('more/', TemplateView.as_view(template_name="more_about.html")),
 ]
 
